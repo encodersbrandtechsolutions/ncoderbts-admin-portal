edac93559a4fbb4be5d887652352b1327771002b
Rahul Rajpal <rahulrajpal2911@gmail.com>
2022-08-02 10:54:33 +0530

    Changed to ng serve

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY . .
 RUN rm -rf /var/lib/jenkins/workspace/encoders-brandtech-web-admin/node_modules
 
 RUN npm i
-RUN npm run build --project=astro-pujeets-web-admin
+RUN npm run build-astro-pujeets-web-admin
 
 EXPOSE 3004
 
--- projects/astro-pujeets-web-admin/src/app/app.module.ts
+++ projects/astro-pujeets-web-admin/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EncodersBrandtechSharedLibraryModule } from 'encoders-brandtech-shared-library';
+import { EncodersBrandtechSharedLibraryModule } from 'encoders-module-library';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
