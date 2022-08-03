5bea814b3d4f33f7282ab1dc3064279983e33a13
Rahul Rajpal <rahulrajpal2911@gmail.com>
2022-08-04 00:15:13 +0530

    Base path updated

--- projects/astro-pujeets-web-admin/src/app/app.component.ts
+++ projects/astro-pujeets-web-admin/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'astro-pujeets-web-admin';
+
+  constructor(router: Router) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.navigateByUrl('/auth/sign-in');
+    }
+  }
 }
